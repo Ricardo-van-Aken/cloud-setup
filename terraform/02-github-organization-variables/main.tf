00cae176a6070d21720cbacd01374f86da92b5e8
Zeepaardje <rvaken98@gmail.com>
2025-10-16 21:02:49 +0200

    dweqdfew

--- terraform/02-github-organization-variables/main.tf
+++ terraform/02-github-organization-variables/main.tf
@@ -37,10 +37,10 @@ data "terraform_remote_state" "do_foundation" {
 
 # Organization secrets
 # Expose DigitalOcean Spaces CI credentials as GitHub Organization secrets
-resource "github_actions_organization_secret" "spaces_access_key_ci" {
-  secret_name     = "DO_SPACES_ACCESS_KEY_CI"
+resource "github_actions_organization_variable" "spaces_access_key_ci" {
+  variable_name     = "DO_SPACES_ACCESS_KEY_CI"
   visibility      = "private"
-  plaintext_value = data.terraform_remote_state.do_foundation.outputs.bucket_spaces_access_key_ci
+  value = data.terraform_remote_state.do_foundation.outputs.bucket_spaces_access_key_ci
 }
 
 resource "github_actions_organization_secret" "spaces_secret_key_ci" {
